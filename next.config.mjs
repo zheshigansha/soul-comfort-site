ee8950b90f3b5d7fd2e08f7747406062232c552b
Rain <zheshigansha@gmail.com>
2025-07-01 14:41:53 +0800

    修复Next.js配置和Navbar组件

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+};
 
 export default withNextIntl(nextConfig);
