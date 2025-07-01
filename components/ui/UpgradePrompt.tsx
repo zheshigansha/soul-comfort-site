e91a265ab6d91a1262c0a0b73654b682583b63a4
Rain <zheshigansha@gmail.com>
2025-07-01 21:52:44 +0800

    修复: 为UpgradePrompt组件添加onClose参数类型定义

--- components/ui/UpgradePrompt.tsx
+++ components/ui/UpgradePrompt.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import { useClient } from '@/app/[locale]/providers';
 
-export default function UpgradePrompt({ onClose }) {
+export default function UpgradePrompt({ onClose }: { onClose: () => void }) {
   const { clientId } = useClient();
   const [selectedTab, setSelectedTab] = useState('subscription');
   const [isLoading, setIsLoading] = useState(false);
